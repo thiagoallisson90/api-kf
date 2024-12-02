b4c99d1315f51ae529c7015d3b6c6797109392c8
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-12-02 16:13:41 -0300

    Refactor: add try-catch to create

--- src/controllers/DataController.js
+++ src/controllers/DataController.js
@@ -1,36 +1,41 @@
 import dataService from "../services/DataService.js";
 
 const create = async (req, res) => {
-  const {
-    device_name,
-    temperature,
-    humidity,
-    luminosity,
-    rssi,
-    sent,
-    lat,
-    long,
-  } = req.body;
+  try {
+    const {
+      device_name,
+      temperature,
+      humidity,
+      luminosity,
+      rssi,
+      sent,
+      lat,
+      long,
+    } = req.body;
 
-  if (
-    !device_name ||
-    !temperature ||
-    !humidity ||
-    !luminosity ||
-    !rssi ||
-    !sent ||
-    !lat ||
-    !long
-  ) {
-    res.status(400).json({ error: "Data format is invalid!" });
+    if (
+      !device_name ||
+      !temperature ||
+      !humidity ||
+      !luminosity ||
+      !rssi ||
+      !sent ||
+      !lat ||
+      !long
+    ) {
+      res.status(400).json({ error: "Data format is invalid!" });
+    }
+
+    const data = await dataService.create(req.body);
+
+    res.status(201).json({
+      msg: "Sensored data inserted successfully!",
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).json(error);
   }
-
-  const data = await dataService.create(req.body);
-
-  res.status(201).json({
-    msg: "Sensored data inserted successfully!",
-    data,
-  });
 };
 
 const getAllData = async (req, res) => {
