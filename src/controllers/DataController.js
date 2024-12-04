b6eaa2b51cc5fb88cce09786d73eb883dbccff88
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-12-04 17:19:23 -0300

    update version

--- src/controllers/DataController.js
+++ src/controllers/DataController.js
@@ -2,26 +2,30 @@ import dataService from "../services/DataService.js";
 
 const create = async (req, res) => {
   try {
-    const {
-      device_name,
-      temperature,
-      humidity,
-      luminosity,
-      rssi,
-      sent,
-      lat,
-      long,
-    } = req.body;
+    const device_name = req.body.device_name || undefined;
+    const temperature = !isNaN(req.body.temperature)
+      ? parseFloat(req.body.temperature)
+      : undefined;
+    const humidity = !isNaN(req.body.humidity)
+      ? parseFloat(req.body.humidity)
+      : undefined;
+    const luminosity = !isNaN(req.body.luminosity)
+      ? parseFloat(req.body.luminosity)
+      : undefined;
+    const rssi = !isNaN(req.body.rssi) ? parseFloat(req.body.rssi) : undefined;
+    const sent = !isNaN(req.body.sent) ? parseFloat(req.body.sent) : undefined;
+    const lat = !isNaN(req.body.lat) ? parseFloat(req.body.lat) : undefined;
+    const long = !isNaN(req.body.long) ? parseFloat(req.body.long) : undefined;
 
     if (
-      !device_name ||
-      !temperature ||
-      !humidity ||
-      !luminosity ||
-      !rssi ||
-      !sent ||
-      !lat ||
-      !long
+      device_name == undefined ||
+      temperature == undefined ||
+      humidity == undefined ||
+      luminosity == undefined ||
+      rssi == undefined ||
+      sent == undefined ||
+      lat == undefined ||
+      long == undefined
     ) {
       res.status(400).json({ error: "Data format is invalid!" });
     }
